82bb463f780348e747674d0518935a0b949e3134
Timeless Residents <business@satotakuya.jp>
2025-02-04 00:17:34 +0900

    Initial Commit

---
+++ pedometer/pedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pedometerApp.swift
+//  pedometer
+//
+//  Created by sato takuya on 2025/02/04.
+//
+
+import SwiftUI
+
+@main
+struct pedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pedometerTests/pedometerTests.swift
@@ -0,0 +1,17 @@
+//
+//  pedometerTests.swift
+//  pedometerTests
+//
+//  Created by sato takuya on 2025/02/04.
+//
+
+import Testing
+@testable import pedometer
+
+struct pedometerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
